0a566e68363bf6acba2c5ce8c5494d9e6f8023e2
Andrea <collet.andrea@gmail.com>
2011-11-22 22:55:38 +0100

    Create le classi e ciò che i loro oggetti rappresentano.

--- Classe.rb
+++ Classe.rb
@@ -1,11 +1,18 @@
-class Bunny
-	def furnicate
+# Andrea_20111122_ex01_
+
+# Every mammal can walk and eat salad
+class Mammal
+	def walk
 	
 	end
 end
 
-class Mammal
-	def
+# Every bunny is a mammal, also can jump and eat carrots
+class Bunny < mammal
+	def jump
 	
 	end
 end
+
+bugsBunny = Bunny.new
+
