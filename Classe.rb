9ab424524dac263053c478e009268336be281e9e
Andrea <collet.andrea@gmail.com>
2011-11-22 23:27:03 +0100

    Added methods actions and created objects.

--- Classe.rb
+++ Classe.rb
@@ -1,18 +1,31 @@
-# Andrea_20111122_ex01_
+# Andrea_20111122_ex01
 
-# Every mammal can walk and eat salad
+# Every mammal can walk and eat
 class Mammal
 	def walk
+		puts "Stomp stomp"
+	end
 	
+	def eat
+		puts "Crunch crunch"
 	end
 end
 
-# Every bunny is a mammal, also can jump and eat carrots
+# Every bunny is a mammal, also can jump and bite carrots
 class Bunny < mammal
 	def jump
-	
+		puts "Hoop hoop!"
+	end
+
+	def bite
+		puts "Crunch crunch!"
 	end
 end
 
-bugsBunny = Bunny.new
+# We create three types of objects, a mammal and two bunnies
+animal		= Mammal.new
+normalBunny	= Bunny.new
+bugsBunny	= Bunny.new
 
+# Bugs Bunny can also talk, beause it's special
+bugsBunny.talk()
