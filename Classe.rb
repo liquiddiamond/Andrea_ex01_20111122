832164ab35132d75a143e37c3ab34aef55a0c82d
Andrea <collet.andrea@gmail.com>
2011-11-23 09:46:00 +0100

    Correzione metodi e oggetti.

--- Classe.rb
+++ Classe.rb
@@ -2,23 +2,23 @@
 
 # Every mammal can walk and eat
 class Mammal
-	def walk
-		puts "Stomp stomp"
+	def walk()
+		puts "Stomp Stomp!"
 	end
 	
-	def eat
-		puts "Crunch crunch"
+	def eat()
+		puts "Yum Yum!"
 	end
 end
 
 # Every bunny is a mammal, also can jump and bite carrots
-class Bunny < mammal
-	def jump
-		puts "Hoop hoop!"
+class Bunny < Mammal
+	def jump()
+		puts "Hop Hop!"
 	end
 
-	def bite
-		puts "Crunch crunch!"
+	def bite()
+		puts "Crunch Crunch!"
 	end
 end
 
@@ -27,5 +27,24 @@ animal		= Mammal.new
 normalBunny	= Bunny.new
 bugsBunny	= Bunny.new
 
-# Bugs Bunny can also talk, beause it's special
-bugsBunny.talk()
+# Bugs Bunny can also talk, because it's special
+def bugsBunny.talk()
+	puts("Hey dude wassup?")
+end
+
+# Now let's see what they can do
+puts "animal"
+animal.walk()
+animal.eat()
+
+puts "\n\nnormalBunny"
+normalBunny.walk()
+normalBunny.eat()
+normalBunny.jump()
+normalBunny.bite()
+
+puts "\n\nbugsBunny:"
+bugBunny.walk()
+bugBunny.eat()
+bugBunny.jump()
+bugBunny.bite()
