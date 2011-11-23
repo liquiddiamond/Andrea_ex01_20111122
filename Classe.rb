6dcf02ec90bbc9c84b6fc5a5e7aca159245e09cb
Andrea <collet.andrea@gmail.com>
2011-11-23 10:35:49 +0100

    Creazione dei commenti ai metodi

--- Classe.rb
+++ Classe.rb
@@ -1,40 +1,61 @@
 # Andrea_20111122_ex01
 
+
+###################
+#   Mammal class
+###################
+
+# Mammal is a class
 # Every mammal can walk and eat
 class Mammal
 	def self.walk
 		puts(self "started to walk")
-		puts (walks for yeld() kilometers)
+		puts ("walks for " yeld " kilometers")
 		puts(self "ended the walk")
 	end
 	
 	def eat
 		puts(self "eat start!")
-		yeld()
+		puts ("eats for " yeld " kilometers")
 		puts("eat end")
 	end
 end
 
+
+###################
+#   Bunny class
+###################
+
+# Bunny is a Mammal subclass
 # Every bunny is a mammal, also can jump and bite carrots
 class Bunny < Mammal
 	def self.jump
 		puts("jump start!")
-		yeld()
+		yeld
 		puts("jump end")
 	end
 
 	def bite
 		puts("bite start!")
-		yeld()
+		yeld
 		puts("bite end")
 	end
 end
+
+
+###################
+#   create instances
+###################
 
 # We create three types of objects, a mammal and two bunnies
 animal		= Mammal.new
 normalBunny	= Bunny.new
 bugsBunny	= Bunny.new
 
+
+###################
+#   create instance method
+###################
 
 # Bugs Bunny can also talk, because it's special
 def bugsBunny.talk
@@ -44,18 +65,28 @@ def bugsBunny.talk
 end
 
 
-# This is an array of distances walked by animal
+###################
+#   animal
+###################
+
+# Array of distances walked by animal
 distances[1, 4, 6, 8, 3, 11]
+# Array of things which animal can eat
+food["salad", "tomato", "cheese"]
 
-# This is an array of things which 
-
-# Now let's see what did
 puts "animal:\n------------"
 animal.walk(distances.inject(0){|tdist,pdist| tdist + pdist})
-animal.eat()
+animal.eat(puts)
 
-# This is an array of distances walked by normalBunny
+
+###################
+#   normalBunny
+###################
+
+# Array of distances walked by normalBunny
 distances[4, 7, 5, 42]
+# Array of things which normalBunny can eat
+food["mushroom", "bread", "honey", "potato", "grass"]
 
 puts "\n\nnormalBunny:\n------------"
 normalBunny.walk(distances.inject(0){|tdist,pdist| tdist + pdist})
@@ -63,8 +94,15 @@ normalBunny.eat()
 normalBunny.jump()
 normalBunny.bite()
 
-# This is an array of distances walked by bugsBunny
+
+###################
+#   bugsBunny
+###################
+
+# Array of distances walked by bugsBunny
 distances[23, 67, 45, 89]
+# Array of things which bugsBunny can eat
+food["carrot", "cucumber", "champagne", "caviar"]
 
 puts "\n\nbugsBunny:\n------------"
 bugsBunny.walk(distances.inject(0){|tdist,pdist| tdist + pdist})
