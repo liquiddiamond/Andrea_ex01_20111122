e3d4db02fd986563e78af74ceb11ed46334e29cc
Andrea <collet.andrea@gmail.com>
2011-11-23 10:22:55 +0100

    Creazione del blocco da passare al primo metodo di classe.

--- Classe.rb
+++ Classe.rb
@@ -2,23 +2,31 @@
 
 # Every mammal can walk and eat
 class Mammal
-	def walk()
-		puts "Stomp Stomp!"
+	def self.walk
+		puts(self "started to walk")
+		puts (walks for yeld() kilometers)
+		puts(self "ended the walk")
 	end
 	
-	def eat()
-		puts "Yum Yum!"
+	def eat
+		puts(self "eat start!")
+		yeld()
+		puts("eat end")
 	end
 end
 
 # Every bunny is a mammal, also can jump and bite carrots
 class Bunny < Mammal
-	def jump()
-		puts "Hop Hop!"
+	def self.jump
+		puts("jump start!")
+		yeld()
+		puts("jump end")
 	end
 
-	def bite()
-		puts "Crunch Crunch!"
+	def bite
+		puts("bite start!")
+		yeld()
+		puts("bite end")
 	end
 end
 
@@ -27,24 +35,39 @@ animal		= Mammal.new
 normalBunny	= Bunny.new
 bugsBunny	= Bunny.new
 
+
 # Bugs Bunny can also talk, because it's special
-def bugsBunny.talk()
-	puts("Hey dude wassup?")
+def bugsBunny.talk
+	puts("eat start!")
+	yeld()
+	puts("eat end")
 end
 
-# Now let's see what they can do
-puts "animal"
-animal.walk()
+
+# This is an array of distances walked by animal
+distances[1, 4, 6, 8, 3, 11]
+
+# This is an array of things which 
+
+# Now let's see what did
+puts "animal:\n------------"
+animal.walk(distances.inject(0){|tdist,pdist| tdist + pdist})
 animal.eat()
 
-puts "\n\nnormalBunny"
-normalBunny.walk()
+# This is an array of distances walked by normalBunny
+distances[4, 7, 5, 42]
+
+puts "\n\nnormalBunny:\n------------"
+normalBunny.walk(distances.inject(0){|tdist,pdist| tdist + pdist})
 normalBunny.eat()
 normalBunny.jump()
 normalBunny.bite()
 
-puts "\n\nbugsBunny:"
-bugBunny.walk()
-bugBunny.eat()
-bugBunny.jump()
-bugBunny.bite()
+# This is an array of distances walked by bugsBunny
+distances[23, 67, 45, 89]
+
+puts "\n\nbugsBunny:\n------------"
+bugsBunny.walk(distances.inject(0){|tdist,pdist| tdist + pdist})
+bugsBunny.eat()
+bugsBunny.jump()
+bugsBunny.bite()
